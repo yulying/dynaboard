df13587a171cf23554abce0fbb18f9072ed1ddf3
yulying <yu.ying.9013@gmail.com>
2024-07-16 10:58:23 -0400

    Remove deleted directories

--- server/middleware/error.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).json({ msg: err.message })
-    } else {
-        res.status(500).json({ msg: err.message })
-    }
-}
-
-export default errorHandler
--- server/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res, next) => {
-    const error = new Error('Not Found')
-    error.status = 404
-    next(error)
-}
-
-export default notFound
