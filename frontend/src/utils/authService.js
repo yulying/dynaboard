75fad67e023479f314eb9075d7990b004a418610
yulying <yu.ying.9013@gmail.com>
2024-07-18 14:38:56 -0400

    See above

--- frontend/src/utils/authService.js
+++ frontend/src/utils/authService.js
@@ -1,4 +1,4 @@
-const API_URL = `/${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/`;
+const API_URL = `https://${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/`;
 
 class AuthService {
     async register(username, email, password) {
